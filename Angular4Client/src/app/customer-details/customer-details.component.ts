1ba4b599989886d7901ba20d2fc82d953e0ef5b5
Rodriguez <oscar.s.rodriguez@accenturefederal.com>
2019-10-23 09:31:28 -0500

    initial commit

---
+++ Angular4Client/src/app/customer-details/customer-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+import { Customer } from '../customer';
+import { DataService } from '../data.service';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'customer-detail',
+  templateUrl: './customer-details.component.html',
+  styleUrls: ['./customer-details.component.css'],
+  providers: [DataService]
+})
+
+export class CustomerDetailsComponent {
+
+  @Input() customer: Customer;
+
+  constructor(private dataService: DataService) {}
+
+  delete(): void {
+    this.dataService.delete(this.customer.id).then(() => this.goBack());
+  }
+
+  goBack(): void {
+    window.location.replace('');
+  }
+}
---
+++ Angular4Client/src/app/customer.ts
@@ -0,0 +1,6 @@
+export class Customer {
+    public id: number;
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+}
